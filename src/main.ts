6c7890b0ce46b6094228270321dd9b25e2639461
Will Knowles <will@primerapi.com>
2021-01-28 13:35:48 +0100

    use ts-node to run

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,14 @@ import StripeConnection from './Stripe';
    */
   const { requestExamples, connection } = StripeConnection;
 
+  console.log(`Authorizing payment using "${connection.name}"`);
+
   const authRequest = requestExamples.auth();
   const authResponse = await connection.authorize(authRequest);
+
+  console.log(
+    `Authorization request complete: "${authResponse.transactionStatus}"`,
+  );
 
   if (authResponse.transactionStatus === 'FAILED') {
     console.log(`Authorization Request failed: ${authResponse.errorMessage}`);
@@ -27,15 +33,25 @@ import StripeConnection from './Stripe';
     processorTransactionId: authResponse.processorTransactionId,
   };
 
+  console.log(
+    `Processor transaction ID: ${transactionInfo.processorTransactionId}`,
+  );
+
   /**
    * Uncomment the following lines to capture
    */
+  // console.log(`Capturing payment using "${connection.name}"`);
   // const captureRequest = requestExamples.capture(transactionInfo);
   // const captureResponse = await connection.capture(captureRequest);
+  // console.log(
+  //   `Capture request complete "${captureResponse.transactionStatus}"`,
+  // );
 
   /**
    * Uncomment the following lines to cancel
    */
+  // console.log(`Cancelling payment using "${connection.name}"`);
   // const cancelRequest = requestExamples.cancel(transactionInfo);
   // const cancelResponse = await connection.cancel(cancelRequest);
+  // console.log(`Cancel request complete "${cancelResponse.transactionStatus}"`);
 })();
